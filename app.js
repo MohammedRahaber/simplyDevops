631d4b70593a7de7c9f8301017321d8aa97afc1b
Mohammed Rahaber <rahberm@gmail.com>
2021-11-29 10:56:38 -0800

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const path = require('path')
+const app=express();
+
+app.use('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'','index.html'))
+})
+app.listen(500,()=>console.log('server running........'))
